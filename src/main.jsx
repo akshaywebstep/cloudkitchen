4c416ecca4c5addac8e0179f2832c092fc7f0995
shikhawebstep@gmail.com <khuranakaran2000@gmail.com>
2026-08-06 17:17:48 +0530

    update all

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/demo/cloud-kitchen-dashboard">
     <App />
   </BrowserRouter>
 );
